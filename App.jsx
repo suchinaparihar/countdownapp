97f8ac85ef23b192ce6033f527e681705fe9a363
Suchina Parihar <suchina.parihar@gmail.com>
2018-12-14 16:17:41 -0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Clock from './Clock';
+import './App.css';
+import {Form, FormControl,Button } from 'react-bootstrap';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      deadline: 'December 25, 2018',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline(){
+
+    this.setState({deadline:this.state.newDeadline});
+  }
+
+  render(){
+    return(
+      //for css 'className' keyword is used
+      <div className="App">
+     <div className = "App-title"> Countdown to {this.state.deadline}</div>
+
+     <Clock deadline = {this.state.deadline}/>
+
+
+      <Form inline>
+      <FormControl
+       className = "Deadline-input"
+       placeholder= 'new date'
+       onChange={event => this.setState({newDeadline: event.target.value})}
+      />
+      <Button onClick={() => this.changeDeadline()}>
+        Submit
+      </Button>
+     </Form>
+    </div>
+
+  )
+  }
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; //Current directory that is why '.' is used before the name
+
+ReactDOM.render(
+  <App> </App>, document.getElementById('root')
+);
